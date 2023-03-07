b86f71efb7bf6b13599fe6a4f7d09564bb7e68cd
daniel <ant8090@gmail.com>
2023-03-07 13:31:06 +0800

    update template

--- controllers/etcdcluster_controller.go
+++ controllers/etcdcluster_controller.go
@@ -17,27 +17,26 @@ limitations under the License.
 package controllers
 
 import (
-    "context"
-    "fmt"
-    dbv1beta1 "github.com/imind-lab/etcd-operator/api/v1beta1"
-    "github.com/imind-lab/etcd-operator/controllers/utils"
-    appsv1 "k8s.io/api/apps/v1"
-    corev1 "k8s.io/api/core/v1"
-    v1 "k8s.io/api/discovery/v1"
-    "k8s.io/apimachinery/pkg/api/errors"
-    "k8s.io/apimachinery/pkg/runtime"
-    "k8s.io/apimachinery/pkg/types"
-    ctrl "sigs.k8s.io/controller-runtime"
-    "sigs.k8s.io/controller-runtime/pkg/client"
-    "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-    "sigs.k8s.io/controller-runtime/pkg/log"
-    "strconv"
+	"context"
+	"fmt"
+	dbv1beta1 "github.com/imind-lab/etcd-operator/api/v1beta1"
+	"github.com/imind-lab/etcd-operator/controllers/utils"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"strconv"
 )
 
 // EtcdClusterReconciler reconciles a EtcdCluster object
 type EtcdClusterReconciler struct {
-    client.Client
-    Scheme *runtime.Scheme
+	client.Client
+	Scheme *runtime.Scheme
 }
 
 //+kubebuilder:rbac:groups=db.imind.tech,resources=etcdclusters,verbs=get;list;watch;create;update;patch;delete
@@ -58,103 +57,94 @@ type EtcdClusterReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *EtcdClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-    logger := log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-    fmt.Println("EtcdClusterReconciler Reconcile")
+	fmt.Println("EtcdClusterReconciler Reconcile")
 
-    etcdCluster := &dbv1beta1.EtcdCluster{}
-    if err := r.Get(ctx, req.NamespacedName, etcdCluster); err != nil {
-        logger.Error(err, "The etcd cluster has been deleted")
-        return ctrl.Result{}, client.IgnoreNotFound(err)
-    }
+	etcdCluster := &dbv1beta1.EtcdCluster{}
+	if err := r.Get(ctx, req.NamespacedName, etcdCluster); err != nil {
+		logger.Error(err, "The etcd cluster has been deleted")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-    service := utils.NewService(etcdCluster)
-    if err := controllerutil.SetControllerReference(etcdCluster, service, r.Scheme); err != nil {
-        return ctrl.Result{}, err
-    }
+	service := utils.NewService(etcdCluster)
+	if err := controllerutil.SetControllerReference(etcdCluster, service, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
 
-    svc := &corev1.Service{}
-    slice := v1.EndpointSlice{}
-    slice.Endpoints
-    if err := r.Get(ctx, types.NamespacedName{Name: etcdCluster.Name, Namespace: etcdCluster.Namespace}, svc); err != nil {
-        if errors.IsNotFound(err) {
-            if err := r.Create(ctx, service); err != nil {
-                logger.Error(err, "create service failed")
-                return ctrl.Result{}, err
-            }
-        } else {
-            return ctrl.Result{}, err
-        }
-    }
+	svc := &corev1.Service{}
+	if err := r.Get(ctx, types.NamespacedName{Name: etcdCluster.Name, Namespace: etcdCluster.Namespace}, svc); err != nil {
+		if errors.IsNotFound(err) {
+			if err := r.Create(ctx, service); err != nil {
+				logger.Error(err, "create service failed")
+				return ctrl.Result{}, err
+			}
+		} else {
+			return ctrl.Result{}, err
+		}
+	}
 
-    configMap := utils.NewConfigMap(etcdCluster)
-    if err := controllerutil.SetControllerReference(etcdCluster, configMap, r.Scheme); err != nil {
-        return ctrl.Result{}, err
-    }
+	configMap := utils.NewConfigMap(etcdCluster)
+	if err := controllerutil.SetControllerReference(etcdCluster, configMap, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
 
-    cm := &corev1.ConfigMap{}
-    if err := r.Get(ctx, types.NamespacedName{Name: etcdCluster.Name, Namespace: etcdCluster.Namespace}, cm); err != nil {
-        if errors.IsNotFound(err) {
-            if err := r.Create(ctx, configMap); err != nil {
-                logger.Error(err, "create configmap failed")
-                return ctrl.Result{}, err
-            }
-            cm = configMap
-        }
-    }
-    index := int(etcdCluster.Spec.Size) - 1
-    po := &corev1.Pod{}
-    err := r.Get(ctx, types.NamespacedName{Name: etcdCluster.Name + "-" + strconv.Itoa(index), Namespace: etcdCluster.Namespace}, po)
-    if err == nil {
-        if po.Status.Phase == corev1.PodRunning {
-            cm.Data["cluster-state"] = "existing"
-            if err := r.Update(ctx, cm); err != nil {
-                logger.Error(err, "update configmap failed")
-            }
-        }
-    }
+	cm := &corev1.ConfigMap{}
+	if err := r.Get(ctx, types.NamespacedName{Name: etcdCluster.Name, Namespace: etcdCluster.Namespace}, cm); err != nil {
+		if errors.IsNotFound(err) {
+			if err := r.Create(ctx, configMap); err != nil {
+				logger.Error(err, "create configmap failed")
+				return ctrl.Result{}, err
+			}
+			cm = configMap
+		}
+	}
+	index := int(etcdCluster.Spec.Size) - 1
+	po := &corev1.Pod{}
+	err := r.Get(ctx, types.NamespacedName{Name: etcdCluster.Name + "-" + strconv.Itoa(index), Namespace: etcdCluster.Namespace}, po)
+	if err == nil {
+		if po.Status.Phase == corev1.PodRunning {
+			cm.Data["cluster-state"] = "existing"
+			if err := r.Update(ctx, cm); err != nil {
+				logger.Error(err, "update configmap failed")
+			}
+		}
+	}
 
-    statefulSet := utils.NewStatefulSet(etcdCluster)
-    if err := controllerutil.SetControllerReference(etcdCluster, statefulSet, r.Scheme); err != nil {
-        return ctrl.Result{}, err
-    }
+	statefulSet := utils.NewStatefulSet(etcdCluster)
+	if err := controllerutil.SetControllerReference(etcdCluster, statefulSet, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
 
-    sts := &appsv1.StatefulSet{}
-    if err := r.Get(ctx, types.NamespacedName{Name: etcdCluster.Name, Namespace: etcdCluster.Namespace}, sts); err != nil {
-        if errors.IsNotFound(err) {
-            if err := r.Create(ctx, statefulSet); err != nil {
-                logger.Error(err, "create statefulset failed")
-                return ctrl.Result{}, err
-            }
-        } else {
-            return ctrl.Result{}, err
-        }
-    } else {
-        if *sts.Spec.Replicas != *statefulSet.Spec.Replicas || sts.Spec.Template.Spec.Containers[0].Image != statefulSet.Spec.Template.Spec.Containers[0].Image {
-            if *sts.Spec.Replicas != *statefulSet.Spec.Replicas {
-                cm.Data["last-size"] = strconv.Itoa(int(*sts.Spec.Replicas))
-                if err := r.Update(ctx, cm); err != nil {
-                    logger.Error(err, "update configmap failed")
-                }
-            }
+	sts := &appsv1.StatefulSet{}
+	if err := r.Get(ctx, types.NamespacedName{Name: etcdCluster.Name, Namespace: etcdCluster.Namespace}, sts); err != nil {
+		if errors.IsNotFound(err) {
+			if err := r.Create(ctx, statefulSet); err != nil {
+				logger.Error(err, "create statefulset failed")
+				return ctrl.Result{}, err
+			}
+		} else {
+			return ctrl.Result{}, err
+		}
+	} else {
+		if *sts.Spec.Replicas != *statefulSet.Spec.Replicas || sts.Spec.Template.Spec.Containers[0].Image != statefulSet.Spec.Template.Spec.Containers[0].Image {
+			*sts.Spec.Replicas = *statefulSet.Spec.Replicas
+			sts.Spec.Template.Spec.Containers[0].Image = statefulSet.Spec.Template.Spec.Containers[0].Image
+			if err := r.Update(ctx, sts); err != nil {
+				logger.Error(err, "update statefulset failed")
+				return ctrl.Result{}, err
+			}
+		}
+	}
 
-            *sts.Spec.Replicas = *statefulSet.Spec.Replicas
-            sts.Spec.Template.Spec.Containers[0].Image = statefulSet.Spec.Template.Spec.Containers[0].Image
-            if err := r.Update(ctx, sts); err != nil {
-                logger.Error(err, "update statefulset failed")
-                return ctrl.Result{}, err
-            }
-        }
-    }
-
-    return ctrl.Result{}, nil
+	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *EtcdClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
-    return ctrl.NewControllerManagedBy(mgr).
-        For(&dbv1beta1.EtcdCluster{}).
-        Owns(&appsv1.StatefulSet{}).
-        Owns(&corev1.Service{}).
-        Complete(r)
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&dbv1beta1.EtcdCluster{}).
+		Owns(&appsv1.StatefulSet{}).
+		Owns(&corev1.Service{}).
+		Complete(r)
 }
